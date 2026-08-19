493c7b024b40a378fcc5f28104b763c0099a5d4f
Aditya-aplha <bandraladitya5544@gmail.com>
2026-08-19 17:01:45 +0530

    deploying

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const connectDB = require("./db")
 const app = express()
 
 const corsOptions = {
-    origin: [`http://localhost:5173`,`https://map-nine-rosy.vercel.app`],
+    origin: [`http://localhost:5173`,`https://mappo-delta.vercel.app`],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true
 }
